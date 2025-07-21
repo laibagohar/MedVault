bd87aeb76e8e80aefbccd55fdc7388d9f2f51fab
Saviza-Latif <savizalatif2610@gmail.com>
2025-07-21 13:44:15 +0500

    User model added

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-User = sequelize.define('User', {
+const User = sequelize.define('User', {
   id: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
