fdaa6fe51f61b2040f28e3391e807eb6eaae4ad9
Saviza-Latif <savizalatif2610@gmail.com>
2025-07-21 13:37:39 +0500

    User model added

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+User = sequelize.define('User', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  DOB: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  gender:{
+    type: DataTypes.ENUM('male', 'female', 'other'),
+    allowNull: true,
+  }
+}, {
+  timestamps: true, // createdAt and updatedAt will be managed automatically
+  tableName: 'users', // PostgreSQL table name
+});
+
+
+
+module.exports = User;
