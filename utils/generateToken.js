b9801417e5346ec6786e62289606d8bd8011fb42
moazamrathore <moazamrathore18@gmail.com>
2025-07-22 14:27:38 +0500

    Added signup functionality in userController, token generation in utlis/generateToken and token verification in authMiddleware.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,1 +1,12 @@
 // Token generation utility will go here
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => {
+  return jwt.sign(
+    { id },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRE || '30d' }
+  );
+};
+
+export default generateToken;
