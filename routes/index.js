10a8a97c4b7e294b046c1f9ad25a2457f3be995a
Saviza-Latif <savizalatif2610@gmail.com>
2025-07-23 12:47:13 +0500

    package.json updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import userRoutes from "./userRoutes.js";
+import reportRoutes from "./reportRoutes.js";
 import express from "express";
 const apiRoutes = express.Router();
 
 apiRoutes.use(userRoutes);
+apiRoutes.use(reportRoutes);
 
 export default apiRoutes;
