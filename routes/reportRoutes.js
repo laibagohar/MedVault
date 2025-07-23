bb03051d124b717af1965608248e6460c9823f6c
Saviza-Latif <savizalatif2610@gmail.com>
2025-07-23 12:47:53 +0500

    feature upload report added

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,1 +1,10 @@
-// Report routes will go here
+import express from 'express';
+import { uploadReport } from '../controllers/reportController.js';
+import { upload } from '../utils/uploadfile.js';
+
+const router = express.Router();
+
+// Route to upload a PDF report
+router.post('/upload', upload.single('file'), uploadReport);
+
+export default router;
