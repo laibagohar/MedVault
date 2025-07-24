247f18d60a94115dc46c6b9a30e76f5fb25c49db
moazamrathore <moazamrathore18@gmail.com>
2025-07-24 14:12:23 +0500

    changed token expire time to 1h

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -5,7 +5,7 @@ const generateToken = (id) => {
   return jwt.sign(
     { id },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRE || '30d' }
+    { expiresIn: process.env.JWT_EXPIRE || '1h' }
   );
 };
 
